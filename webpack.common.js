3a3cd0e8b95c1a1d37a9830512fa6e6c382e4772
nacereal <n5rehman@uwaterloo.ca>
2022-05-23 00:24:38 -0400

    Setup base files and webpack configs

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Library App",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ca]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          "css-loader", 
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.(?:png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+    ],
+  },
+};
